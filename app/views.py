bbe5dac6b79f555fb2a9af640c723280d0c914c1
Sruti <sruti@gatech.edu>
2018-01-12 14:54:57 -0500

    basic login functionality

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, redirect, session
+from flask import render_template, redirect, session, request
 
 @app.route('/')
 def index():
@@ -8,13 +8,30 @@ def index():
     else:
         member_type = session.get('member_type')
         if member_type == 'coord':
-            return redirect('coordinator_home')
+            return redirect('coordinator/home')
         else:
-            return redirect('member_home')
+            return redirect('member/home')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'user' and password == 'pass':
+            session['login'] = True
+            session['member_type'] = 'coord'
+            member_type = 'coord'
+            if member_type == 'coord':
+                return redirect('coordinator/home')
+            else:
+                return redirect('member/home')
+    return render_template('login.html', error=False)
+
+@app.route('/logout')
+def logout():
+    session['login'] = False
+    session['member_type'] = None
+    return redirect('login')
 
 # ==================================================  MEMBER   =====================================================
 
@@ -23,7 +40,7 @@ def member():
     if not session.get('login'):
         return redirect('login')
     else:
-        return redirect('member_home')
+        return redirect('member/home')
 
 @app.route('/member/home')
 def member_home():
@@ -36,7 +53,7 @@ def coordinator():
     if not session.get('login'):
         return redirect('login')
     else:
-        return redirect('coordinator_home')
+        return redirect('coordinator/home')
 
 @app.route('/coordinator/home')
 def coordinator_home():
