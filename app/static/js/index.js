83d391cf29b82b2b60404290fabf688cdaa6253a
Sruti <sruti@gatech.edu>
2018-01-23 16:15:13 -0500

    started on add member

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     var location = window.location.pathname.split("/")[2];
     $(".active").removeClass("active");
     $("#" + location).addClass("active");
+
+    if (location == 'members') {
+        $("#memberModalLabel").modal('hide');
+    }
 });
