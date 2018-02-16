474e310f6a10337498977fe0ed7632173dcdcb66
Sruti <sruti@gatech.edu>
2018-02-16 15:49:29 -0500

    finalize enrollment form (add number, add children) and comment code

--- app/static/js/coordinator/members.js
+++ app/static/js/coordinator/members.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
     sessionStorage.profile_picture = '/static/images/profile_pictures/default_profile_pic.png'
 });
 
+
+// close member modal and clear all fields in the general form
 function closeModal() {
     sessionStorage.profilePicIsDataURI = false;
     sessionStorage.profile_picture = '/static/images/profile_pictures/default_profile_pic.png'
@@ -14,18 +16,14 @@ function closeModal() {
     $("#show-profile-img").attr('src', "#");
     $("#crop-success-button").hide();
     $("#member-profile-pic").attr('src', '/static/images/profile_pictures/default_profile_pic.png');
-    $("#general-form")[0].reset();
-    $("#username").val("");
-    $("#password").val("");
-    $("#join_date").val("");
-    $("#club_name").val("");
-    $("#commitment_pledge").val("");
-    $("#photo_release").val("");
     $.post( "members/clear_new_member", function() {
       console.log( "successfully requested clear session" );
       })
       .done(function(data) {
         console.log( "successfully cleared session" );
+        $(".member-modal-nav-item-active").removeClass("member-modal-nav-item-active");
+        $("#general").addClass("member-modal-nav-item-active");
+        $("#member-modal-body-component").html(data);
       })
       .fail(function() {
         console.log( "error clearing session" );
@@ -35,6 +33,7 @@ function closeModal() {
       });
 }
 
+// save new member, send profile pic info to the server
 function saveModal() {
     saveMemberToStorage(sessionStorage.currentPage);
     var profile_pic_blob;
@@ -64,6 +63,8 @@ function saveModal() {
     });
 }
 
+// change page on member modal. Save info from the current page, send to server to temporarily save
+// in session, then go to new page
 function memberModalChange(e) {
     var id = $(e).attr("id");
     var memberData = saveMemberToStorage(sessionStorage.currentPage);
@@ -88,6 +89,7 @@ function memberModalChange(e) {
       });
 }
 
+// determine which page needs to be saved to storage
 function saveMemberToStorage(page) {
     var updateMember;
     if (page == 'general') {
@@ -104,6 +106,7 @@ function saveMemberToStorage(page) {
     return updateMember;
 }
 
+// save the general form
 function saveGeneral() {
     var generalObj = {};
     var generalValues = $("#general-form").serializeArray();
@@ -113,55 +116,33 @@ function saveGeneral() {
     return generalObj;
 }
 
+// save the enrollment form
 function saveEnrollmentForm() {
     var enrollementFormObj = {};
+    var enrollmentValues = $("#enrollment-form").serializeArray();
+    console.log(enrollmentValues);
     return enrollementFormObj;
 }
 
+// save the demographic data form
 function saveDemographicData() {
     var demographicDataObj = {};
     return demographicDataObj;
 }
 
+// save the self sufficiency matrix form
 function saveSelfSufficiencyMatrix() {
     var selfSufficiencyObj = {};
     return selfSufficiencyObj;
 }
 
+// save the self efficacy quiz form
 function saveSelfEfficacyQuiz() {
     var selfEfficacyObj = {};
     return selfEfficacyObj;
 }
 
-function cropImage() {
-    var cropped = $("#show-profile-img").cropper('getCroppedCanvas').toDataURL('image/jpeg', 0.8);
-    $("#member-profile-pic").attr('src', cropped);
-    sessionStorage.profilePicIsDataURI = true;
-    sessionStorage.profile_picture = cropped;
-    $("#show-profile-img").cropper('destroy');
-    $("#show-profile-img").attr('src', "#");
-    $("#crop-success-button").hide();
-}
-
-function readURL(input) {
-    $("#show-profile-img").cropper('destroy');
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-        $('#show-profile-img').attr('src', e.target.result);
-        $('#show-profile-img').cropper({
-          aspectRatio: 10 / 10,
-          scalable: false,
-          zoomOnTouch: false,
-          zoomable:false
-        });
-    }
-        reader.readAsDataURL(input.files[0]);
-        $("#crop-success-button").show();
-    }
-}
-
-
+// convert a data URI to a blob to send to the server
 function dataURItoBlob(dataURI) {
   dataURI = dataURI.replace(/\s/g, '');
   // convert base64 to raw binary data held in a string
@@ -186,4 +167,56 @@ function dataURItoBlob(dataURI) {
   var blob = new Blob([ab], {type: mimeString});
   return blob;
 
+}
+
+// ===================================== general ===============================================
+
+// when the crop button is clicked, get the cropped canvas and save it to
+// local storage as a data URI. Destroy the cropper and hide the crop button.
+function cropImage() {
+    var cropped = $("#show-profile-img").cropper('getCroppedCanvas').toDataURL('image/jpeg', 0.8);
+    $("#member-profile-pic").attr('src', cropped);
+    sessionStorage.profilePicIsDataURI = true;
+    sessionStorage.profile_picture = cropped;
+    $("#show-profile-img").cropper('destroy');
+    $("#show-profile-img").attr('src', "#");
+    $("#crop-success-button").hide();
+}
+
+// when an image is uploaded, read in the url of the image and put it into the cropper
+// display the cropper and the crop button
+function readURL(input) {
+    $("#show-profile-img").cropper('destroy');
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+        $('#show-profile-img').attr('src', e.target.result);
+        $('#show-profile-img').cropper({
+          aspectRatio: 10 / 10,
+          scalable: false,
+          zoomOnTouch: false,
+          zoomable:false
+        });
+    }
+        reader.readAsDataURL(input.files[0]);
+        $("#crop-success-button").show();
+    }
+}
+
+// ===================================== enrollment form ===============================================
+
+function addPhoneNumber() {
+    $("#member-phone-numbers").append('<div class="form-group row"><input name="phone_numbers" type="tel" placeholder="Phone Number" class="input input-sm uw-input col-sm-4 phone-number"><button onclick="removeNum(this)" class="btn btn-sm uw-button"><span class="glyphicon glyphicon-remove"></span></button></div>');
+}
+
+function removeNum(e) {
+    var parent = $(e).parent();
+    $(parent).remove();
+}
+
+function addChild() {
+    var child = $("#member-children-table .member-child:first-child").html();
+    var child_num = parseInt($("#member-children-table .member-child:last-child .child-num").html());
+    $("#member-children-table").append('<tr class="member-child">' + child + '</tr>');
+    $("#member-children-table .member-child:last-child .child-num").html(child_num + 1);
 }
