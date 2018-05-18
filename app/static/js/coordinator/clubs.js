bb6ae3a858232f299e6c12984c94c2107a879b9e
Sruti <sruti@gatech.edu>
2018-05-18 14:49:49 -0400

    basic add club modal, tried jqueryui and bootstrap tokenizer, don't need either

--- app/static/js/coordinator/clubs.js
+++ app/static/js/coordinator/clubs.js
@@ -1,9 +1,32 @@
+$(document).ready(function() {
+// TODO: figure this out !!
+//    $('#tokenfield').tokenfield({
+//      autocomplete: {
+//        source: ['red','blue','green','yellow','violet','brown','purple','black','white'],
+//        delay: 100
+//      },
+//      showAutocompleteOnFocus: true
+//    })
+})
+
 function showClubDetails(club) {
     $("#club-details").show();
 }
 
 function hideClubDetails() {
     $("#club-details").hide();
+}
+
+function openClubModal() {
+// TODO: figure this out !!
+//    $("#tokenfield").tokenfield();
+//    $('#tokenfield').tokenfield({
+//      autocomplete: {
+//        source: ['red','blue','green','yellow','violet','brown','purple','black','white'],
+//        delay: 100
+//      },
+//      showAutocompleteOnFocus: true
+//    });
 }
 
 function closeClubModal() {
