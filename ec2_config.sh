7ceaaea2a0a8f59cae8df2ab5717bbccfe887188
Sruti Guhathakurta <sruti@gatech.edu>
2018-10-16 15:51:03 -0400

    new db

--- ec2_config.sh
+++ ec2_config.sh
@@ -6,7 +6,8 @@ cd uw_database
 pip install --user -r requirements.txt
 nohup python run.py --host 0.0.0.0 > uw_database.out 2>&1 &
 # route port 80 to port 8090 (http)
-sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
+sudo iptables -
+A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -i eth0 -p tcp --dport 8090 -j ACCEPT
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8090
 # route port 443 to port 8090 (https)
@@ -17,4 +18,3 @@ sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-p
 # ps -ef | grep nohup
 # then kill process
 # kill <pid>
-
