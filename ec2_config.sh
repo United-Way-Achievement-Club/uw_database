2c93c614e17f8afa6ab1f87499bfdd4065c31080
Sruti Guhathakurta <sruti@gatech.edu>
2018-10-16 14:37:58 -0400

    edit goal should work properly

--- ec2_config.sh
+++ ec2_config.sh
@@ -5,7 +5,16 @@ git clone -b dev https://github.com/SrutiG/uw_database.git
 cd uw_database
 pip install --user -r requirements.txt
 nohup python run.py --host 0.0.0.0 > uw_database.out 2>&1 &
+# route port 80 to port 8090 (http)
+sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -i eth0 -p tcp --dport 8090 -j ACCEPT
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8090
+# route port 443 to port 8090 (https)
+sudo iptables -A INPUT -i eth0 -p tcp --dport 443 -j ACCEPT
+sudo iptables -A INPUT -i eth0 -p tcp --dport 8090 -j ACCEPT
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8090
 # to kill process, get process id using command
 # ps -ef | grep nohup
 # then kill process
 # kill <pid>
+
