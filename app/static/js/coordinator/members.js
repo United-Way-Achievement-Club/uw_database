65249ae56254fb49300970647c63ee6ecbd7b14f
Brian <brianbll9@gmail.com>
2019-06-05 23:52:04 -0400

    Coordinators - Members search and sort features

--- app/static/js/coordinator/members.js
+++ app/static/js/coordinator/members.js
@@ -684,3 +684,21 @@ function viewQuiz(e) {
     }
 
  }
+
+ // Search for Member
+function search() {
+  var input, filter, li, a, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value;
+  li = $('.members-list-item');
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByClassName("members-list-item-wrapper")[1];
+      txtValue = a.textContent || a.innerText;
+      anyCase = txtValue.toLowerCase();
+      if (anyCase.indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
