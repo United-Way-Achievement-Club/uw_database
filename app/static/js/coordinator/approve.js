61bdf79e0d069157ca030b4d151a96b487ccc913
SrutiG <sruti@gatech.edu>
2019-07-02 13:58:34 -0400

    approve.js comments

--- app/static/js/coordinator/approve.js
+++ app/static/js/coordinator/approve.js
@@ -1,3 +1,12 @@
+/**
+ * Set the status of a proof based on input from coordinator.
+ * Communicate with the server to update the proof status in
+ * the database.
+ * @param username member's username
+ * @param proof_name name of the proof
+ * @param step_name name of the step
+ * @param status status (approved, denied)
+ */
 function set_document_status(username, proof_name, step_name, status) {
     prompt_text = status == "approved" ? "(optional) Reason for approval" : "Reason for denial";
     reason = window.prompt(prompt_text, "No reason entered");
@@ -19,6 +28,10 @@ function set_document_status(username, proof_name, step_name, status) {
     }
 }
 
+/**
+ * download a file (file found in an s3 link)
+ * @param link the link to the file
+ */
 function download(link) {
     window.open(link, "_blank");
 }
